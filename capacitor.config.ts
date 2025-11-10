609f5864699649163914b20bdad887092a70de0d
bauyrzhai.mustafaev <bauyrzhai.mustafaev@1win.pro>
2025-11-10 23:14:28 +0500

    add template of splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -15,6 +15,22 @@ const config: CapacitorConfig = {
       style: KeyboardStyle.Light,
       resizeOnFullScreen: true,
     },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
   },
 };
 
