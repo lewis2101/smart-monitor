9a07fdec96febc3355f2dafd407e1fea0e918021
bauyrzhai.mustafaev <bauyrzhai.mustafaev@1win.pro>
2025-11-11 13:27:59 +0500

    fix learning layout

--- src/pages/learning.vue
+++ src/pages/learning.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { IonHeader, IonPage, IonToolbar, type RefresherCustomEvent, useIonRouter } from "@ionic/vue";
+import { IonHeader, IonPage, type RefresherCustomEvent, useIonRouter } from "@ionic/vue";
 import DefaultLayoutHeader from "@/components/layout/default-layout-header.vue";
 import { MainTabRoutes } from "@/router/router-list.ts";
 import BaseContentWithRefresher from "@/components/base/base-content-with-refresher/base-content-with-refresher.vue";
 import LearningLinkedInfoBlock from "@/components/learning/learning-linked-info-block/learning-linked-info-block.vue";
 import { useStatusBarColor } from "@/composables/native/use-status-bar-color.ts";
+import BaseToolbar from "@/components/base/base-toolbar/base-toolbar.vue";
 
 const { setSecondaryColor } = useStatusBarColor();
 setSecondaryColor();
@@ -27,11 +28,11 @@ const handleRefresh = (event: RefresherCustomEvent) => {
 </script>
 
 <template>
-  <ion-page>
+  <ion-page class="learning-page">
     <ion-header>
-      <ion-toolbar class="new-order-page__toolbar">
+      <base-toolbar>
         <default-layout-header title="Обучение" close @click-close="handleClickClose" @click-back="handleClickClose" />
-      </ion-toolbar>
+      </base-toolbar>
     </ion-header>
     <base-content-with-refresher @refresh="handleRefresh">
       <div class="learning-page__body">
@@ -44,11 +45,6 @@ const handleRefresh = (event: RefresherCustomEvent) => {
 <style scoped lang="scss">
 .learning-page {
   padding-top: env(safe-area-inset-top);
-
-  &__toolbar {
-    --padding-top: 0;
-    --padding-bottom: 0;
-  }
 
   &__body {
     padding: 16px 24px;
